431b02f2ff7b12357f33f29fca634435426f560f
Jonathan Hamberg <jonathanhamberg@gmail.com>
2018-02-26 15:02:00 -0800

    Simple example of using the ThingSpeak.  API Write Key has been removed.  Refer to slack for API Write Key.

---
+++ ArduinoThingSpeak/ArduinoThingSpeak.ino
@@ -0,0 +1,77 @@
+/*
+    This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+
+    You need to get streamId and privateKey at data.sparkfun.com and paste them
+    below. Or just customize this script to talk to other HTTP servers.
+
+*/
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "SealUW";
+const char* password = "uwsealuwseal";
+
+// Use WiFiClient class to create TCP connections
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+
+
+  Serial.print("connecting to ");
+
+  if (!client.connect("api.thingspeak.com", 80)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  updateThingSpeak(String("field1=") + 234 + "&field2=" + 1234 + "&field3=" + 431);
+
+  delay(20000);
+}
+
+void updateThingSpeak(String tsData) {
+  if (client.connect("api.thingspeak.com", 80)) {
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: ................\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+    client.print(tsData);
+
+    if (client.connected()) {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+    }
+  }
+}
+
+
